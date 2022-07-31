1bc63a18455c2295965ce85a4ba3fc0077985647
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-08-01 00:48:55 +0530

    setup permission handling just to work with this temp project

--- app/src/main/java/com/jatinvashisht/viewtopdfconverterdemo/MainActivity.kt
+++ app/src/main/java/com/jatinvashisht/viewtopdfconverterdemo/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.jatinvashisht.viewtopdfconverterdemo
 
-import android.graphics.pdf.PdfDocument
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.jatinvashisht.viewtopdfconverterdemo.ui.theme.ViewToPDFConverterDemoTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,9 +34,81 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val permissionState = rememberMultiplePermissionsState(
+                        permissions = listOf(
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        )
+                    )
 
+                    val context = LocalContext.current
+                    SideEffect {
+                        if (!permissionState.allPermissionsGranted) {
+                            permissionState.launchMultiplePermissionRequest()
+                        }
+                    }
+
+                    val allPermissionsGiven = requestPermission(permissionState = permissionState)
+//                    val permissions = listOf<String>(
+//                        Manifest.permission.READ_EXTERNAL_STORAGE,
+//                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+//                    )
+//                    permissions.forEach { permission ->
+//                        val isPermissionGranted = isPermissionGranted(permission, context)
+//                        Log.d("mainscreen", "is permission granted is $isPermissionGranted")
+//                        allPermissionsGiven = allPermissionsGiven && isPermissionGranted
+//                    }
+                    if (allPermissionsGiven) {
+                        MainUi()
+                    }
                 }
             }
         }
     }
+}
+
+
+@Composable
+fun MainUi() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun requestPermission(permissionState: MultiplePermissionsState): Boolean {
+    permissionState.permissions.forEach { perm ->
+        when (perm.permission) {
+            Manifest.permission.READ_EXTERNAL_STORAGE -> {
+                when (perm.status) {
+                    is PermissionStatus.Denied -> {
+
+                    }
+                    PermissionStatus.Granted -> {
+
+                    }
+                }
+            }
+            Manifest.permission.WRITE_EXTERNAL_STORAGE -> {
+                when (perm.status) {
+                    is PermissionStatus.Denied -> {
+
+                    }
+                    PermissionStatus.Granted -> {
+
+                    }
+                }
+            }
+        }
+    }
+    val allPermissionsGranted = permissionState.allPermissionsGranted
+    return allPermissionsGranted
+}
+
+private fun isPermissionGranted(permission: String, context: Context): Boolean {
+    return ContextCompat.checkSelfPermission(
+        context,
+        permission
+    ) == PackageManager.PERMISSION_GRANTED
 }
--- app/src/main/java/com/jatinvashisht/viewtopdfconverterdemo/ui/theme/Theme.kt
+++ app/src/main/java/com/jatinvashisht/viewtopdfconverterdemo/ui/theme/Theme.kt
@@ -5,11 +5,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(139, 40, 32, 255),
+    onPrimary = Color.White,
+    primaryVariant = Color(143, 44, 37, 255),
+    secondary = Color(180, 54, 44, 255),
+    onSecondary = Color.White,
+    background = Color(37, 19, 18, 255),
+    onBackground = Color.White,
+    surface = Color(24, 80, 111, 255),
+    onSurface = Color.White
 )
 
 private val LightColorPalette = lightColors(
